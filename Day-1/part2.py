8294ef4485dbe1ccaef4dc6c9a8950ec2d32c618
Archan Patkar <pat@archanpatkar.com>
2020-12-01 11:35:56 +0530

    done

---
+++ Day-1/part2.py
@@ -0,0 +1,6 @@
+data = open("input.txt","r").read();
+data = [int(row) for row in data.split("\n")];
+print(data);
+sums = [(n1,n2,n3) for n1 in data for n2 in data for n3 in data if(n1 != n2 != n3 and (n1+n2+n3 == 2020))]
+print(sums);
+print([t[0]*t[1]*t[2] for t in sums]);
