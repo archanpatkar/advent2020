2269c60048a44235b4015f37e4dea4f65ad774fd
Archan Patkar <pat@archanpatkar.com>
2020-12-03 13:29:38 +0530

    utils

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from functools import *
 
 def p(str):
     pprint(str,indent=4)
@@ -10,8 +11,23 @@ def freq(str):
         else: l[c] = 1
     return l
 
-def aoci(func=lambda x: x):
+def a(list,index):
+    if index < len(list): return list[index]
+    print("beyond {} list boundary!".format(index))
+
+def m(list,index):
+    return list[index % len(index)]
+
+def add(list):
+    return reduce(lambda acc,v: acc+v,list,0)
+
+def mult(list):
+    return reduce(lambda acc,v: acc*v,list,1)
+
+iden = lambda x: x
+
+def aoci(func=iden):
     return func(open("input.txt","r").read());
 
-def fnl(func=lambda x:x):
+def fnl(func=iden):
     return [func(l) for l in aoci(lambda data: data.split("\n"))]
