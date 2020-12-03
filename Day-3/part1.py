17ff66bdbef2e4321ea7043c9a0f3fe8c9676eb6
Archan Patkar <pat@archanpatkar.com>
2020-12-03 11:08:32 +0530

    done

--- Day-3/part1.py
+++ Day-3/part1.py
@@ -2,5 +2,17 @@ import sys
 sys.path.append("..")
 from common import *
 
-data = fnl();
+def parse(line):
+    return line*100
+
+data = fnl(parse);
 p(data);
+
+treecount = 0
+x = 0
+y = 0
+while y < len(data) and x < len(data[0]):
+    if data[y][x] == "#": treecount += 1
+    x += 3
+    y += 1
+print(treecount)
