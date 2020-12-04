fe74d371abf8b0e61e2c15bae35ae0d6c4f6cb87
Archan Patkar <pat@archanpatkar.com>
2020-12-04 11:30:21 +0530

    done

--- Day-4/part1.py
+++ Day-4/part1.py
@@ -3,7 +3,32 @@ sys.path.append("..")
 from common import *
 
 def parse(line):
-    return line
+    line = line.split("\n");
+    passports = []
+    buff = ""
+    for l in line:
+        if not len(l):
+            passports.append(buff)
+            buff = ""
+        else:
+            buff += " {}".format(l.strip())
+    passports.append(buff)
+    np = []
+    for p in passports:
+        kv = p.strip().split(" ");
+        passp = {}
+        for e in kv:
+            temp = e.split(":")
+            passp[temp[0]] = temp[1]
+        np.append(passp)
+    return np
 
-data = fnl(parse);
+data = aoci(parse);
 p(data);
+
+keys = ["byr","iyr","eyr","hgt","hcl","ecl","pid","cid"]
+valid = 0
+for passp in data:
+    if len(passp.keys()) == 8 or (len(passp.keys()) == 7 and not("cid" in passp)):
+        valid += 1
+print(valid)
