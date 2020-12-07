fb64703d7931bebc457e2f4e92315f2f80acee61
Archan Patkar <pat@archanpatkar.com>
2020-12-07 12:16:21 +0530

    done

--- Day-7/part1.py
+++ Day-7/part1.py
@@ -4,7 +4,27 @@ sys.path.append("..")
 from common import *
 
 def parse(data):
-    return data
+    d = data[:-1].split("contain")
+    return (d[0].strip(),[c.strip() for c in d[1].split(",")])
 
 data = fnl(parse);
-p(data);
+graph = {}
+for n in data:
+    graph[n[0][:-4].strip()] = [e.split(" ",1)[1][:-4].strip() for e in n[1]]
+p(graph)
+
+def rec_count(graph,node,visited=[]):
+    p = []
+    for n in graph:
+        for b in graph[n]:
+            if(b == node):
+                p.append(n)
+                break
+    for n in p:
+        if not (n in visited):
+            rec_count(graph,n,visited)
+            visited.append(n)
+    return len(visited)
+
+o = rec_count(graph,"shiny gold")
+print(o)
