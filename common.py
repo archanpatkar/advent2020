6894ab10aca0185cce7747877a2b7536bfae250a
Archan Patkar <pat@archanpatkar.com>
2020-12-07 20:02:29 +0530

    update

--- common.py
+++ common.py
@@ -1,5 +1,9 @@
 from pprint import pprint
 from functools import *
+from collections import *
+from math import *
+from itertools import *
+from graphviz import Graph, Digraph
 
 def p(str):
     pprint(str,indent=4)
@@ -19,6 +23,9 @@ def kvl(data,sep=" ",s=":"):
         temp = e.split(s)
         p.append((temp[0],temp[1]))
     return p
+
+def lval(data,sep=","):
+    return data.split(sep);
 
 def periodic(data,pd=lambda x:not len(x),sep="\n",app=" "):
     lines = data.split(sep);
@@ -40,6 +47,49 @@ def freq(str):
         else: l[c] = 1
     return l
 
+point = namedtuple("Point2D",["x", "y"]);
+
+UP = (0,-1)
+DOWN = (0,1)
+LEFT = (-1,0)
+RIGHT = (1,0)
+
+n4l = [
+            UP,  
+    LEFT,          RIGHT, 
+            DOWN  
+]
+
+n8l = [
+    (-1,-1), UP,  (1,-1), 
+    LEFT,         RIGHT, 
+    (-1,1), DOWN, (1,1)
+]
+
+ng = lambda ns: lambda p: tuple((point(p.x+d[0],p.y+d[1]) for d in ns))
+n4 = ng(n4l)
+n8 = ng(n8l)
+
+def drawgrid(points,xs,ys,pc="*",em=".",jo="\n"):
+    strmap = []
+    for y in range(ys):
+        cr = []
+        for x in range(xs):
+            if (x,y) in points:
+                cr.append(pc)
+            else: cr.append(em)
+        strmap.append(cr)
+    return strmap.join(jo)
+
+def eucdis(p1,p2):
+    return sqrt(((p2.x-p1.x) ** 2) + ((p2.y-p1.y) ** 2))
+
+def chebdis(p1,p2):
+    return max(abs(p2.x-p1.x),abs(p2.y-p1.y))
+
+def manhdis(p1,p2):
+    return abs(p1.x-p2.x) + abs(p1.y-p2.y)
+
 def a(list,index):
     if index < len(list): return list[index]
     print("beyond {} list boundary!".format(index))
@@ -60,6 +110,11 @@ def sw(str,e):
     return str.startswith(e)
 
 nums = ["0","1","2","3","4","5","6","7","8","9"]
+
+def ks(dict):
+    k = list(dict.keys())
+    if " " in k: return len(k)-1;
+    return len(k)
 
 def alpha(str,cutoff="z",start="a"):
     n = ord(str)
@@ -86,6 +141,9 @@ def bf(v,s,e):
 def bs(v,s,e):
     return v > s and v <= e;
 
+def checkeg(ns,xlen,ylen,ch=bf):
+    return tuple(filter(lambda p: ch(p.x,0,xlen) and ch(p.y,0,ylen), ns))
+
 def forall(l,f):
     return reduce(lambda acc,v: acc and f(v),l,True)
 
@@ -103,6 +161,83 @@ def flol(l,sep=","):
     return [float(v) for v in l]
 
 iden = lambda x: x
+
+def grid(data,sep=""):
+    return list(map(lambda l: l.split(sep),data.split("\n")));
+
+# wip
+# assume l[n][1] is mod
+def crt(l):
+    ms = [m[1] for m in l]
+    k = len(ms)
+    N = mult(ms)
+    y = [N/m for m in ms]
+    z = [(y[i] ** -1) % ms[i] for i in range(k)]
+    x = [l[i][0]*y[i]*z[i] for i in range(k)]
+    return (add(x),N,add(x)%N)
+
+def unionfind(*lists):
+    ss = enumerate(map(lambda l: set(l),lists))
+    ops = map(lambda p: set(map(lambda e: (e,p[0]),p[1])), ss)
+    return reduce(lambda acc,v: acc.union(v),ops,set())
+    
+def bfs(graph,start):
+    visited = {start:0}
+    parents = {start:-1}
+    q = [start]
+    while len(q) != 0:
+        curr = q.pop(0)
+        p(curr)
+        for adj in graph.get(curr):
+            if not(adj in visited):
+                q.append(adj);
+                visited[adj] = visited[curr] + 1
+                parents[adj] = curr
+    return (visited,parents)
+
+def dfs(graph,start,cb=lambda p,c:(p,c),visited={}):
+    visited[start] = 1
+    for adj in graph[start]:
+        if not visited.get(adj):
+            cb(start,adj)
+            dfs(graph,adj,cb,visited)
+
+# shortest path bfs
+def spbfs(graph,start,end):
+    visited, parents = bfs(graph,start)
+    if(end in visited):
+        path = []
+        curr = end
+        while curr != start:
+            path.append(curr)
+            curr = parents[curr]
+        path.append(start)
+        path.reverse()
+        return path
+    print("No path!")
+
+def toposort(graph):
+    visited = {}
+    ts = []
+    for n in graph:
+        if not(n in visited):
+            dfs(graph,n,lambda p,c:ts.append((p,c)),visited)
+    return ts
+
+def drawGraph(path,graph,process=iden):
+    g = Graph(format='svg',name=path)
+    for n in graph:
+        for e in graph[n]:
+            g.edge(process(n),process(e))
+    g.render()
+
+def drawDGraph(path,graph,process=iden):
+    g = Digraph(format='svg',name=path)
+    for n in graph:
+        for e in graph[n]:
+            t = process(e)
+            g.edge(process(n),t[1],label=str(t[0]))
+    g.render()
 
 def aoci(func=iden):
     return func(open("input.txt","r").read());
