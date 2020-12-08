e21ba2baa765f528e9914a1eee4098e1a241310a
Archan Patkar <pat@archanpatkar.com>
2020-12-08 12:43:14 +0530

    done

--- Day-8/part1.py
+++ Day-8/part1.py
@@ -1,10 +1,24 @@
 import sys
-import math
 sys.path.append("..")
 from common import *
 
 def parse(data):
-    return data
+    data = data.split(" ")
+    return (data[0],int(data[1]))
 
 data = fnl(parse);
-p(data);
+
+def machine(code,memory={"acc":0,"ip":0}):
+    done = []
+    while memory["ip"] < len(code):
+        if(memory["ip"] in done): break
+        done.append(memory["ip"])
+        ins = code[memory["ip"]]
+        if(ins[0] == "acc"): memory["acc"] += ins[1]
+        elif(ins[0] == "jmp"): 
+            memory["ip"] += ins[1]
+            continue;
+        memory["ip"] += 1
+    return memory
+
+p(machine(data))
