c7f35f6a688bc99002f6a16cceb7c781d1f4c034
Archan Patkar <pat@archanpatkar.com>
2020-12-09 11:04:44 +0530

    done

--- Day-9/part1.py
+++ Day-9/part1.py
@@ -3,7 +3,19 @@ sys.path.append("..")
 from common import *
 
 def parse(data):
-    return data
+    return int(data)
 
 data = fnl(parse);
-p(data)
+p(data);
+
+preamble = 25
+i = preamble
+while i < len(data):
+    last25sum = set()
+    for j in data[i-preamble:i]:
+        for k in data[i-preamble:i]:
+            last25sum.add(j+k)
+    if data[i] in last25sum:
+        i += 1
+        continue;
+    break;
