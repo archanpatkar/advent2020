f2f5139df9754e56eae290dfecb451fe842ea1ba
Archan Patkar <pat@archanpatkar.com>
2020-12-15 12:53:17 +0530

    done

--- Day-15/part2.py
+++ Day-15/part2.py
@@ -2,8 +2,25 @@ import sys
 sys.path.append("..")
 from common import *
 
-def parse(data):
-    return data
+data = [19,20,14,0,9,1]
+last = [*data]
 
-data = fnl(parse);
-p(data);
+ru = {0:[]}
+for i in range(len(last)):
+    ru[last[i]] = [i+1]
+p(ru)
+
+# Bruteforced it!
+for i in range(len(last),30000000):
+    uses = ru.get(last[-1])
+    if uses != None and len(uses) > 1: 
+        diff = uses[-1] - uses[-2]
+        last.append(diff)
+        t = ru.get(diff)
+        if t != None:
+            t.append(i+1)
+        else: ru[diff] = [i+1]
+    else:
+        last.append(0)
+        ru[0].append(i+1)
+print(last[-1])
